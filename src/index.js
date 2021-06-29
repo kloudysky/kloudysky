c851ea142bf8edfa1a8625e25db144a04b59971f
Adam Kamboj <kamboj.adam@gmail.com>
2021-06-29 06:20:39 -0400

    updated for firebase

--- src/index.js
+++ src/index.js
@@ -213,3 +213,21 @@ $(window).on("load", function () {
     });
   });
 });
+
+//fade in on scroll
+$(window).on("load", function () {
+  /* Every time the window is scrolled ... */
+  $(window).on("scroll", function () {
+    /* Check the location of each desired element */
+    $(".hideme-contact").each(function (i) {
+      const bottom_of_object =
+        $(this).position().top + $(this).outerHeight() * 0.5;
+      const bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ opacity: "1" }, 500);
+      }
+    });
+  });
+});
